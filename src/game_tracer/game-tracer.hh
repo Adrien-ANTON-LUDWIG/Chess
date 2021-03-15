aabebb5485ed5d17af3861d9f5b50c7666277c4b
Adrien Anton Ludwig <adrien.anton-ludwig@epita.fr>
2021-03-15 22:19:12 +0100

    [ADD] listeners
    Co-authored-by: Lucas Kervoal <lucas.kervoal@epita.fr>

---
+++ src/game_tracer/game-tracer.hh
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "board.hh"
+#include "listener.hh"
+#include "pgn-parser.hh"
+
+namespace board
+{
+    using Listener = listener::Listener;
+
+    class GameTracer
+    {
+    public:
+        GameTracer(std::string pgn_filepath,
+                   std::vector<std::string> listeners);
+        ~GameTracer();
+
+        void play_pgn_move(PgnMove &move, bool &color);
+        void play_pgn_game();
+        void interractive();
+
+        bool is_draw_repetition();
+
+    private:
+        Chessboard board_;
+        std::string pgn_filepath_;
+        std::vector<Listener*> listeners_;
+
+        // History
+        std::vector<Chessboard> history_;
+    };
+
+} // namespace board
