3709fa7915494fea406bdade2e1f2b7e0835b4f2
Pierre Pelletier <pierre.pelletier@epita.fr>
2021-03-20 00:00:37 +0100

    Same

--- src/ai/evaluators/simplified.cc
+++ src/ai/evaluators/simplified.cc
@@ -37,7 +37,7 @@ namespace ai
         {
             pieces_sum += board.pieces_[c][p].count() * values[p];
             pieces_sum -=
-                3.0f * board.pieces_[(c + 1) % 2][p].count() * values[p];
+                30.0f * board.pieces_[(c + 1) % 2][p].count() * values[p];
 
             for (size_t i = 0; i < 64; i++)
             {
